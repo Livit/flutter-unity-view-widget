d3956f216474560bed5aa7fc4c5e272d503d5030
Hardian Jatmiko <miko@labster.com>
2020-10-05 21:43:50 +0800

    Use current activity regardless isAR is true or not.
    MOBILE-368

--- android/src/main/java/com/rexraphael/flutterunitywidget/UnityUtils.java
+++ android/src/main/java/com/rexraphael/flutterunitywidget/UnityUtils.java
@@ -50,7 +50,9 @@ public class UnityUtils {
             public void run() {
                 activity.getWindow().setFormat(PixelFormat.RGBA_8888);
 
-                unityPlayer = new UnityPlayer(isAR ? activity : activity.getApplicationContext());
+                // unityPlayer = new UnityPlayer(isAR ? activity : activity.getApplicationContext());
+                // MOBILE-368: AVProVideo plugin for Android needs to use the current activity as context.
+                unityPlayer = new UnityPlayer(activity);
 
                 try {
                     // wait a moument. fix unity cannot start when startup.
